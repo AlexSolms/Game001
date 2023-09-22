a9445dceecf25518d75d5aecaef2ce07a03d21d0
Alex Solms <duaedin@web.de>
2023-09-22 11:49:04 +0200

    begin to move

---
+++ classes/heroKeyboard.class.js
@@ -0,0 +1,7 @@
+class Keyboard {
+    up = false;
+    down = false;
+    left = false;
+    right = false;
+    space = false;
+}
--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ let canvas;
 let world;
 const CANVASWIDTH = 720;
 const CANVASHEIGHT = 400;
+let keyboard = new Keyboard();
 
 
 
@@ -9,5 +10,30 @@ function init() {
     canvas = document.getElementById('canvas')
     world = new World(canvas);
    
-   
+  
 }
+//console.log(keyboard);
+
+window.addEventListener("keydown", (e) =>{
+ (e.code === "ArrowUp" ||  e.code === "KeyW") ? keyboard.up = true : '';
+ (e.code == "ArrowDown" ||  e.code === "KeyS") ? keyboard.down = true : '';
+ (e.code == "ArrowLeft" ||  e.code === "KeyA") ? keyboard.left = true : '';
+ (e.code == "ArrowRight" ||  e.code === "KeyR") ? keyboard.right = true : '';
+ console.log(e.code);
+ console.log(keyboard);
+});
+window.addEventListener("keyup", (e) =>{
+    (e.code === "ArrowUp" ||  e.code === "KeyW") ? keyboard.up = false : '';
+    (e.code == "ArrowDown" ||  e.code === "KeyS") ? keyboard.down = false : '';
+    (e.code == "ArrowLeft" ||  e.code === "KeyA") == "ArrowLeft" ? keyboard.left = false : '';
+    (e.code == "ArrowRight" ||  e.code === "KeyR") == "ArrowRight" ? keyboard.right = false : '';
+    console.log(e.code);
+ console.log(keyboard);
+   });
+
+/* window.onkeydown = (ev) => {
+    console.log('onkeydown: ' + ev.key);
+ };
+ window.onkeyup = function(ev) {
+    console.log('onkeyup: ' + ev.key);
+ }; */
